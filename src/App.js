34f14a905735de9e9e0b04bb88298958fdca4ba2
unknown <mpritchett516@gmail.com>
2018-08-08 18:54:45 -0400

    creat room list and render list of rooms to app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import * as firebase from 'firebase';
 import './App.css';
+import RoomList from './components/RoomList';
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyB3dM9835VxaUgFmvVEb4b9Kq71O5LB5U0",
+    authDomain: "bloc-chat-5dafd.firebaseapp.com",
+    databaseURL: "https://bloc-chat-5dafd.firebaseio.com",
+    projectId: "bloc-chat-5dafd",
+    storageBucket: "bloc-chat-5dafd.appspot.com",
+    messagingSenderId: "499679567203"
+  };
+  firebase.initializeApp(config);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header>
+          <div className="sidebar">
+            <h1>Bloc Chat</h1>
+            <RoomList firebase={ firebase }/>
+          </div>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
